9b798e833cf86d31d0ab84a2faf3e4cd2e09b7ba
alphadzingina <alphaadx6@gmail.com>
2022-02-01 03:34:57 +0100

    select difficulty

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from art import logo
 
 print(logo)
+
+print("Welcome To The Number guessing Game!")
+print("I\'m thinking of a number between 1 and 100.")
+
+attempts = 0
+def select_difficulty():
+    difficulty = input("Choose a difficulty level. Type 'e' for easy or 'h' for hard: ")
+    global attempts
+    if difficulty == 'e':
+        attempts = 10
+    elif difficulty == 'h':
+        attempts = 5
+    else:
+        print("You have chosen an invalid command.")
+        select_difficulty()
+
+select_difficulty()
+
+#print(f"You have {attempts} remaining to guess the correct number")
+
