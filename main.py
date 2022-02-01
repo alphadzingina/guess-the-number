3bcf65cc9a4bd0f2065645bfbc50fb1ae6f6e4c4
alphadzingina <alphaadx6@gmail.com>
2022-02-01 23:02:18 +0100

    changed variable name 'turns' to 'attempts'

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ EASY_LEVEL_ATTEMPTS = 10
 HARD_LEVEL_ATTEMPTS = 5
 
 #Function to check user's guess against actual answer.
-def check_answer(guess, answer, turns):
+def check_answer(guess, answer, attempts):
     """checks answer against guess. Returns the number of turns remaining."""
     if guess > answer:
         print("Too high.")
-        return turns - 1
+        return attempts - 1
     elif guess < answer:
         print("Too low.")
-        return turns - 1
+        return attempts - 1
     else:
         print(f"You got it! The answer was {answer}.")
 
@@ -31,18 +31,18 @@ def game():
     print("I'm thinking of a number between 1 and 100.")
     answer = randint(1, 100)
 
-    turns = set_difficulty()
+    attempts = set_difficulty()
     #Repeat the guessing functionality if they get it wrong.
     guess = 0
     while guess != answer:
-        print(f"You have {turns} attempts remaining to guess the number.")
+        print(f"You have {attempts} attempts remaining to guess the number.")
 
         #Let the user guess a number.
         guess = int(input("Make a guess: "))
 
         #Track the number of turns and reduce by 1 if they get it wrong.
-        turns = check_answer(guess, answer, turns)
-        if turns == 0:
+        attempts = check_answer(guess, answer, attempts)
+        if attempts == 0:
             print("You've run out of guesses, you lose.")
             return
         elif guess != answer:
